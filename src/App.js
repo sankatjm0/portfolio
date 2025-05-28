0d1cb4215cdf9175a53ba068cfba37be3b311555
sankatjm0 <kieudiem2004.nobn@gmail.com>
2025-05-28 16:11:51 +0700

    Create Session Titles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div class='session'>
+      <div name='about-me' id='about'>
+        <h2>ABOUT ME</h2>
+        <p>wanna hear something?</p>
+      </div>
+      <div name='contact' id='contact'>
+        <h2>CONTACT</h2>
+        <p>get in touch w/ me</p>
+      </div>
+      <div class="projects">
+        <h2>PROJECTS</h2>
+        <p>what I've done (probably not my life)</p>
+        <div class="dropdown-content">
+          <a href="#">Item 1</a>
+          <a href="#">Item 2</a>
+          <a href="#">Item 3</a>
+        </div>
+      </div>
+      <div name='ranking' id='ranking'>
+        <h2>RANKING</h2>
+        <p>do you know there's a game?</p>
+      </div>
     </div>
   );
 }
