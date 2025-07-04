3a215116eae747e698babaa3d0cf26288aaba2f9
sankatjm0 <kieudiem2004.nobn@gmail.com>
2025-07-04 09:04:06 +0700

    restart from butterfly and randomobjects

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
     <App />
-  </BrowserRouter>
 );
